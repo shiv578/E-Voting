31e86e2b8a2469715efa68d4e885b8a35794f986
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-04-20 22:45:53 +0530

    Update Contact.jsx

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
 import { motion } from 'framer-motion';
