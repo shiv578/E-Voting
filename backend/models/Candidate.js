b2ade4744cca2c2250de209ce882079914f6fa18
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-04-22 23:14:12 +0530

    Update Candidate.js

--- backend/models/Candidate.js
+++ backend/models/Candidate.js
@@ -1,5 +1,5 @@
  const mongoose = require('mongoose');
-
+ 
 const candidateSchema = new mongoose.Schema({
   election: { type: mongoose.Schema.Types.ObjectId, ref: 'Election', required: true },
   name: { type: String, required: true },
