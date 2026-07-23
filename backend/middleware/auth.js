def0048353299dc1d9986654a13db8a18b671b75
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-23 19:26:19 +0530

    Extract token from Bearer authorization header

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = async function (req, res, next) {  
   const authHeader = req.headers.authorization || '';
+ 
   const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
