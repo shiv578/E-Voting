917dd5eba407ca13b65f2c7ebeafaaf392a711ee
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-23 19:26:09 +0530

    Add token extraction logic in auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
  const User = require('../models/User');  
 
 
+
 module.exports = async function (req, res, next) {  
   const authHeader = req.headers.authorization || '';
   const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
