9cec8c0d2b9319b1c726b1de8c7875e8025fdefc
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-24 19:48:39 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,5 +1,5 @@
 // src/components/Layout.jsx
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; 
 import Navbar from "./Navbar"; 
 import Footer from "./Footer"; 
  
