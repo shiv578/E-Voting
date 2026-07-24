493d9ad707931ab978b05f552e6455efd0b0adfe
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-24 19:48:30 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar"; 
 import Footer from "./Footer"; 
-
+ 
 export default function Layout({ children, hideNavOnAuth = false }) {
   const [theme, setTheme] = useState(() => {
     try {
