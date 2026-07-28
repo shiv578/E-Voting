1b02dfc080778030edb0da5ebce1f54b1d8104d3
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-28 22:55:13 +0530

    Update crypto.js

--- backend/utils/crypto.js
+++ backend/utils/crypto.js
@@ -3,6 +3,7 @@ const crypto = require('crypto')
 
 function genNumericOtp(len = 6) {
   const min = 10 ** (len - 1)
+     
   const max = 10 ** len - 1
   return String(Math.floor(Math.random() * (max - min + 1)) + min)
 }
