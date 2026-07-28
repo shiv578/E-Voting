fd6603d71c0b70ec584a30c065f680f41ec3e33d
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-28 22:51:57 +0530

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const AuditLog = require('../models/AuditLog');
+const AuditLog = require('../models/AuditLog'); 
 const User = require('../models/User');      // 👈 needed for /users
 const auth = require('../middleware/auth');
 
