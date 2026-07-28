41e1e68a7489ee3dc1b3373689f5caabde613775
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-28 22:51:18 +0530

    Update SupportTicket.js

--- backend/models/SupportTicket.js
+++ backend/models/SupportTicket.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const SupportTicketSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true }, 
     email: { type: String, required: true },
     category: { type: String, default: 'other' },
     subject: { type: String, required: true },
