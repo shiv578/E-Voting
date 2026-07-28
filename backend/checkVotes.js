70db35ae9065c2fdcc23173394019ccde0ef6ca8
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-28 22:58:25 +0530

    Update checkVotes.js

--- backend/checkVotes.js
+++ backend/checkVotes.js
@@ -4,6 +4,7 @@ const Vote = require('./models/Vote');
 
 async function run() {
   await mongoose.connect(process.env.MONGODB_URI);
+  
   const votes = await Vote.find().populate('voter candidate election');
   console.log('Votes:', votes);
   await mongoose.disconnect();
