eab73692102d70a8a414a3c47627b86b64cb2e5d
Shivam Swaraj <197034568+shiv578@users.noreply.github.com>
2026-08-03 11:38:56 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
+  
   resetOtpHash: String,
   resetOtpExpires: Date,
   resetOtpAttempts: { type: Number, default: 0 }, 
