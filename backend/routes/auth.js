11304dc3ba8c40b99d82f8f1dadf1e994ea51bab
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-06 22:40:12 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 
  const express = require('express');
 const bcrypt = require('bcryptjs');
+
+
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const multer = require('multer');
