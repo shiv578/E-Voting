b6c5a60a523c9671c0032d8699f121791cf4bba5
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-19 11:27:29 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,6 @@ import api from "../api";
 import { motion, AnimatePresence } from 'framer-motion';
 import { 
 
-
   
   User, CheckCircle, XCircle, Clock, FileText, ChevronRight,
   ArrowRight, ShieldCheck, Zap, Fingerprint, Check, Database, Lock,
