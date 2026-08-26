6b1f1911af970561fc89289c008dde5fe855be43
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-26 13:28:27 +0530

    Update authOtpReset.js

--- backend/routes/authOtpReset.js
+++ backend/routes/authOtpReset.js
@@ -12,6 +12,7 @@ const verifyTurnstile = require("../middleware/verifyTurnstile");
 
 
 
+
 // helper to generate numeric 6-digit OTP
 function generateOtp() {
   return String(Math.floor(100000 + Math.random() * 900000)); 
